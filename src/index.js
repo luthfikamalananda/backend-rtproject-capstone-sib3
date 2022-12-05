e8ebc275248dfb5ad3057171da315e51311d0014
luthfikamalananda <luthfi_kamal_ananda@yahoo.com>
2022-12-06 05:59:07 +0700

    adding corsOptions

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const axios = require('axios');
 
 const cors = require('cors');
 
+const corsOptions = {
+  origin: 'http://localhost:3030',
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
 // Config Defaults Axios dengan Detail Akun Rajaongkir
 axios.defaults.baseURL = 'https://api.rajaongkir.com/starter';
 axios.defaults.headers.common.key = 'a8be5cd808491d7418dd4b76b7884dc6';
@@ -17,7 +22,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.get('/provinsi', async (req, res) => {
   const queryInput = req.query;
